21d171f1551c42a240dce4efe207ea400590fdcf
zettlrobert <zettl.robert@gmail.com>
2022-05-31 16:38:30 +0200

    refactor(utils): mac compatible color parsing

--- utils.sh
+++ utils.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # output-coloring
-export red='\e[0;31m'
-export green='\e[0;32m'
-export lightblue='\e[1;34m'
-export purple='\e[0;35m'
-export cyan='\e[0;36m'
-export lightgreen='\e[1;32m'
-export lightgray='\e[1;37m'
-export NC='\033[0m'
+export red='\x1b[0;31m'
+export green='\x1b[0;32m'
+export lightblue='\x1b[1;34m'
+export purple='\x1b[0;35m'
+export cyan='\x1b[0;36m'
+export lightgreen='\x1b[1;32m'
+export lightgray='\x1b[1;37m'
+export NC='\x1b[0m'
 
 # function to execute a shell command passed as a string
 function execute_command {
@@ -15,8 +15,8 @@ function execute_command {
   exitCode=$?
 
   if [[ ${exitCode} == 0 ]]; then
-    echo -e "\n \n${green}OK:${NC} \t \t ${lightblue}""$1""${NC} successfully executed\n \n"
+    echo -e "${green}OK:${NC} \t \t ${lightblue}""$1""${NC} successfully executed\n"
   else
-    echo -e "\n \n${red}FAIL:${NC} \t \t ${red}""$1""${NC}\n \n"
+    echo -e "${red}FAIL:${NC} \t \t ${red}""$1""${NC}\n"
   fi
 }
